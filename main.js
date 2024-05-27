7e7533849e63a24047ef3df792b0035deb4c9d25
ARFHSI <Abdulraouf.hsi@gmail.com>
2024-05-27 20:03:29 +0300

    lab2

---
+++ main.js
@@ -0,0 +1,38 @@
+let Channel = document.getElementById("tv-show")
+let image = Channel.firstElementChild
+
+function chan(ch){
+    if(ch == 'one'){
+        image.src="https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/blue-planet.jpg"
+    }
+
+    else if (ch == 'two'){
+        image.src="https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/deadpool.jpg"
+        }
+    else if (ch == 'three'){
+            image.src="https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/gravity.jpg"
+            }
+    else if (ch == 'four'){
+                image.src="https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/good-will-hunting.jpg"
+                }   
+    else if (ch == 'five'){
+                    image.src="https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/cnn.jpg"
+                    }   
+    else if (ch == 'six'){
+                        image.src="https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/koyaanisqatsi.jpg"
+                        }   
+    else if (ch == 'seven'){
+                            image.src="https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/parts-unknown.jpg"
+                            }    
+    else if (ch == 'eight'){
+                                image.src="https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/star-wars-solo.jpeg"
+                                }   
+    else if (ch == 'nine'){
+                                    image.src="https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/stranger-things.jpg"
+                                    }
+    
+
+
+
+}
+
